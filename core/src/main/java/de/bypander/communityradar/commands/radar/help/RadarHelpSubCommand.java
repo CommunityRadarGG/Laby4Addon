47fc17ac976e18f648b23c0e1e32e34f7eb59028
Jannis Hecheltjen <jannis.hecheltjen@gmx.de>
2024-06-11 17:20:44 +0200

    Add I18n to the output of /radar help

--- core/src/main/java/de/bypander/communityradar/commands/radar/help/RadarHelpSubCommand.java
+++ core/src/main/java/de/bypander/communityradar/commands/radar/help/RadarHelpSubCommand.java
@@ -5,6 +5,7 @@ import net.labymod.api.Laby;
 import net.labymod.api.client.chat.command.SubCommand;
 import net.labymod.api.client.component.Component;
 import net.labymod.api.client.network.server.ServerData;
+import net.labymod.api.util.I18n;
 
 public class RadarHelpSubCommand extends SubCommand {
 
@@ -18,29 +19,10 @@ public class RadarHelpSubCommand extends SubCommand {
       return false;
 
     StringBuilder sb = new StringBuilder();
-    /**
-     * Dear Labymod-Team,
-     * I didn't use internationalization because the addon only works on GrieferGames, which is a
-     * fully german server where everyone speaks german. Therefore, I didn't find it necessary to
-     * translate the output into English.
-     */
-    sb.append("§8§l§m--------- §8[ §cRadar-Hilfe §8] §8§l§m---------§r\n");
-    sb.append(
-      "§7Custom listen:\n" +
-        "§c/radar listen §7--> §6Zeigt einem alle Listen an.\n" +
-        "§c/radar list add §e<Name> <Prefix> §7--> §6Erstellt eine Liste.\n" +
-        "§c/radar list prefix §e<Name> <Prefix> §7--> §6Ändert den Prefix einer Liste.\n" +
-        "§c/radar list delete §e<Name> §7--> §6Löscht eine Liste.\n" +
-        "§c/radar list show §e<Liste> §7--> §6Zeigt einem alle Spieler einer Liste.\n" +
-        "\n" +
-        "§7Checken:\n" +
-        "§c/radar check §e<Name> §7--> §6Checkt einen Spieler, ob er auf irgendeiner Liste ist.\n" +
-        "§c/radar check * §7--> §6Checkt den ganzen CB und gibt die Spieler aus, auf welcher Liste diese sind.\n" +
-        "\n" +
-        "§7Spieler und Listen:\n" +
-        "§c/radar player add §e<Liste> <Name> <Notizen...> §7--> §6Fügt einen Spieler auf eine Liste hinzu.\n" +
-        "§c/radar player remove §e<Name> §7--> §6entfernt einen Spieler von einer Liste.");
-    sb.append("\n§8§l§m--------- §8[ §cRadar-Hilfe §8] §8§l§m---------§r");
+
+    sb.append(I18n.translate("communityradar.command.help.separator") + "\n");
+    sb.append(I18n.translate("communityradar.command.help.text"));
+    sb.append("\n" + I18n.translate("communityradar.command.help.separator"));
 
     this.displayMessage(Component.text(sb.toString()));
     return true;
